bab88220651ac6489b4cba02375c33681e0b2e8b
Valeria Khristinina <valeria.khristinina@gmail.com>
2024-05-03 13:12:53 +0200

    Add leaflet tile layer

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -9,16 +9,30 @@ export const LEAFLET_OPTIONS = {
     attributionControl: false,
 };
 
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoibGVyYS1raHJpc3RpbmluYSIsImEiOiJjbHZxanc4aGcwMTZ2Mmptcm1xYWFlM2prIn0.ofwmdna6gZxQap7UHJyg7Q";
+
 export default function Map() {
     const [map, setMap] = useState<Leaflet.Map | null>(null);
 
     useEffect(() => {
-        setMap(
-            Leaflet.map('mapid', LEAFLET_OPTIONS).setView(
-                [55.6739075, 12.5692004],
-                20
-            )
-        );
+        setMap(() => {
+            const leafletMap = Leaflet.map("mapid", LEAFLET_OPTIONS).setView(
+              [55.6739075, 12.5692004],
+              20
+            );
+        
+            Leaflet.tileLayer(
+              "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+              {
+                tileSize: 512,
+                zoomOffset: -1,
+                id: "mapbox/streets-v9",
+                accessToken: MAPBOX_TOKEN
+              }
+            ).addTo(leafletMap);
+            return leafletMap;
+          });
     }, []);
 
     return (
