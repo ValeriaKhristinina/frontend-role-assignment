2fe27c4d344f4a8d5292df8b5cfaa7623fa1924e
Valeria Khristinina <valeria.khristinina@gmail.com>
2024-05-03 15:31:00 +0200

    Add players on map

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,28 +1,64 @@
-import { useEffect, useState } from 'react';
-import { LeafletFabricLayer } from './leaflet-extensions.config';
-import * as Leaflet from 'leaflet';
-import { fabric } from 'fabric';
+import { useEffect, useState } from "react";
+import { LeafletFabricLayer } from "./leaflet-extensions.config";
+import * as Leaflet from "leaflet";
+import { fabric } from "fabric";
+import { useSelector } from "react-redux";
+import { getGamePlayers } from "../../redux/game/selectors";
 
 export default function Canvas({ map }: { map: Leaflet.Map }): null {
-    const [fabricCanvas, setFabricCanvas] = useState<fabric.Canvas | null>(
-        null
-    );
-    useEffect(() => {
-        const fabricLayer = new LeafletFabricLayer();
+  const [fabricCanvas, setFabricCanvas] = useState<fabric.Canvas | null>(null);
+  const data = useSelector(getGamePlayers);
 
-        const fabricLayerDidMount = () => {
-            const fabricCanvas = new fabric.Canvas(fabricLayer._canvas);
-            fabricLayer.setFabricCanvas(fabricCanvas);
-            fabricCanvas.requestRenderAll();
-            setFabricCanvas(fabricCanvas);
-        };
-        fabricLayer.delegate({
-            onLayerDidMount: fabricLayerDidMount,
-        });
-        fabricLayer.addTo(map);
-    }, [map]);
+  const getPoint = (lat: number, long: number) => {
+    return map.latLngToLayerPoint([lat, long]);
+  };
 
-    if (!fabricCanvas) return null;
+  useEffect(() => {
+    const fabricLayer = new LeafletFabricLayer();
+    const fabricLayerDidMount = () => {
+      const fabricCanvas = new fabric.Canvas(fabricLayer._canvas);
+      data.map((player) => {
+        const coordinates = getPoint(
+          player.position.latitude,
+          player.position.longitude
+        );
 
-    return null;
+        if (player.teamName === "red") {
+          const rect = new fabric.Rect({
+            left: coordinates.x,
+            top: coordinates.y,
+            fill: "white",
+            width: 20,
+            height: 20,
+            stroke: "red",
+            strokeWidth: 4
+          });
+          fabricCanvas.add(rect);
+        }
+        if (player.teamName === "blue") {
+          const circle = new fabric.Circle({
+            left: coordinates.x,
+            top: coordinates.y,
+            fill: "blue",
+            radius: 12,
+            stroke: "white",
+            strokeWidth: 5
+          });
+          fabricCanvas.add(circle);
+        }
+      });
+      fabricLayer.setFabricCanvas(fabricCanvas);
+      fabricCanvas.requestRenderAll();
+      setFabricCanvas(fabricCanvas);
+    };
+
+    fabricLayer.delegate({
+      onLayerDidMount: fabricLayerDidMount
+    });
+    fabricLayer.addTo(map);
+  }, [map]);
+
+  if (!fabricCanvas) return null;
+
+  return null;
 }
